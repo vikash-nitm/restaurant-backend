fe5e6367ef7fa8ab8e909c82b7e381478c2c7695
vikash-nitm <vikaskumar3966493@gmail.com>
2025-08-20 21:31:05 +0530

    fix: set correct entrypoint and default PORT for Render; main -> server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import app from "./app.js";
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server Running On Port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server Running On Port ${PORT}`);
 });
